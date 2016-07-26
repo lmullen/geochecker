3a86a462650a37e0bacb07400ccac47d7fd43b6e
Lincoln Mullen <lincoln@lincolnmullen.com>
2016-07-26 12:57:14 -0400

    Package documentation

---
+++ R/geochecker-package.r
@@ -0,0 +1,11 @@
+#' geochecker: Check Geocoded Coordinates in a Shiny Gadget
+#'
+#' Given a data frame of latitude and longitude coordinates which have been
+#' geocded from place names or addresses, the Shiny gadget in this package lets
+#' you conveniently check the accuracy of the locations.
+#'
+#' @name geochecker
+#' @docType package
+#'
+#' @importFrom leaflet %>%
+NULL
